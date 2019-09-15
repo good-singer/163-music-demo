e441ad700dca4581099cf3cb41c47f1b1546b27e
good-singer <15732658216@163.com>
2019-09-15 12:03:28 +0800

    song-list实现激活状态

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -17,6 +17,11 @@
       })
       // $(this.el).html(this.template)
     },
+    activeItem(li) {
+      let $li = $(li)
+      $li.addClass('active')
+        .siblings('.active').removeClass('active')
+    },
     clearActive() {
       $(this.el).find('.active').removeClass('active')
     }
@@ -40,15 +45,28 @@
       this.view = view
       this.model = model
       this.view.render(this.model.data)
+      this.bindEvents()
+      this.biindEventHub()
+      this.getAllSongs()
+    },
+    getAllSongs() {
+      this.model.find().then(() => {
+        this.view.render(this.model.data)
+      })
+
+    },
+    bindEvents() {
+      $(this.view.el).on('click', 'li', (e) => {
+        this.view.activeItem(e.currentTarget)
+      })
+    },
+    biindEventHub() {
       window.eventHub.on('upload', () => {
         this.view.clearActive()
       })
       window.eventHub.on('create', (songData) => {
 
         this.model.data.songs.push(songData)
-        this.view.render(this.model.data)
-      })
-      this.model.find().then(() => {
         this.view.render(this.model.data)
       })
 
