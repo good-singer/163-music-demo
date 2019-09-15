517d6bf6712a3e05ba4bd0fd3c308c40fba5b5d6
good-singer <15732658216@163.com>
2019-09-15 11:56:30 +0800

    获取歌曲列表

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -24,6 +24,15 @@
   let model = {
     data: {
       songs: []
+    },
+    find() {
+      let query = new AV.Query('Song')
+      return query.find().then((songs) => {
+        this.data.songs = songs.map((song) => {
+          return { id: song.id, ...song.attributes }
+        })
+        return songs
+      })
     }
   }
   let controller = {
@@ -39,6 +48,10 @@
         this.model.data.songs.push(songData)
         this.view.render(this.model.data)
       })
+      this.model.find().then(() => {
+        this.view.render(this.model.data)
+      })
+
     }
   }
   controller.init(view, model)
