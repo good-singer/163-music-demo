1bc3ec299d8450f2c46e3155af342d76b7c1e1fe
good-singer <15732658216@163.com>
2019-09-17 00:15:33 +0800

    上传加锁

--- src/js/upload-song.js
+++ src/js/upload-song.js
@@ -5,7 +5,11 @@
       return $(this.el).find(selector)[0]
     }
   }
-  let model = {}
+  let model = {
+    data: {
+      status: 'open'
+    }
+  }
   let controller = {
     init(view, model) {
       this.view = view
@@ -39,15 +43,21 @@
               // 文件添加进队列后,处理相关的事情
             });
           },
-          BeforeUpload: function (up, file) {
+          BeforeUpload: (up, file) => {
             // 每个文件上传前,处理相关的事情
             window.eventHub.emit('beforeUpload')
+            if (this.model.data.status === 'closed') {
+              return false
+            } else {
+              this.model.data.status = 'closed'
+              return true
+            }
           },
           UploadProgress: function (up, file) {
             // 每个文件上传时,处理相关的事情
             // uploadStatus.textContent = "上传中";
           },
-          FileUploaded: function (up, file, info) {
+          FileUploaded: (up, file, info) => {
             // 每个文件上传成功后,处理相关的事情
             // uploadStatus.textContent = "上传完毕";
 
@@ -58,6 +68,7 @@
             //  }
             // 参考http://developer.qiniu.com/docs/v6/api/overview/up/response/simple-response.html
             window.eventHub.emit('afterUpload')
+            this.model.data.status = 'open'
 
             var domain = up.getOption("domain");
             var response = JSON.parse(info.response);
